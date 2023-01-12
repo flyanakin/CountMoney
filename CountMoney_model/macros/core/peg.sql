77d1c2b71bdccf4470182865c72fe29a06c6fc79
c9-killer <54168296+flyanakin@users.noreply.github.com>
2023-01-12 16:14:13 +0800

    增加peg计算 (#9)
    Co-authored-by: ziwei

---
+++ CountMoney_model/macros/core/peg.sql
@@ -0,0 +1,12 @@
+{% macro peg(pe, growth, retain) %}
+    {# 计算peg，pe/(增长率*100) retain为保留多少位小数#}
+    (case
+        when {{ growth }} is null then null
+        when {{ pe }} is null then null
+        else
+            round(
+                {{ pe }} / ({{ growth }} * 100),
+                {{ retain }})
+    end
+    )
+{% endmacro %}
--- CountMoney_model/models/marts/stock_picks.sql
+++ CountMoney_model/models/marts/stock_picks.sql
@@ -31,14 +31,36 @@ qi_a_index_defence as (
         {{ receivables_index('accounts_receiv', 'notes_receiv', 'oth_receiv', 'lt_rec', 'total_revenue_ttm') }} as receivables_index,
         net_income_ttm,
         net_income_exclude_minority as net_income_latest,
-        round((total_revenue - total_revenue_last_year)) as growth_total_revenue_yearly,
-        round((net_income_exclude_minority - net_income_last_year)) as growth_net_income_yearly
+        round((total_revenue - total_revenue_last_year)) as total_revenue_yoy,
+        round((net_income_exclude_minority - net_income_last_year)) as net_income_yoy
 
     from balance
     left join income_pivoted
     on balance.stock_code = income_pivoted.stock_code and balance.end_date = income_pivoted.last_end_date
     left join income
     on balance.stock_code = income.stock_code and balance.end_date = income.end_date
+),
+
+growth as (
+    select
+        income.stock_code,
+        income.end_date,
+        {{ ratio('net_income_exclude_minority', 'net_income_last_year',4 ) }} as net_income_yoy_ratio,
+        {{ ratio('total_revenue', 'total_revenue_last_year', 4) }} as total_revenue_yoy_ratio
+    from income
+    left join income_pivoted
+        on income.stock_code = income_pivoted.stock_code
+),
+
+peg as (
+    select
+        growth.stock_code,
+        growth.end_date,
+        {{ peg('pe_ttm', 'net_income_yoy_ratio', 3) }} as peg_by_net_income,
+        {{ peg('pe_ttm', 'total_revenue_yoy_ratio', 3) }} as peg_by_revenue
+    from growth
+    left join daily_index
+        on growth.stock_code = daily_index.stock_code
 ),
 
 final as (
@@ -53,8 +75,12 @@ final as (
         qi_a_index_defence.receivables_index,
         qi_a_index_defence.net_income_ttm,
         qi_a_index_defence.net_income_latest,
-        qi_a_index_defence.growth_total_revenue_yearly,
-        qi_a_index_defence.growth_net_income_yearly,
+        qi_a_index_defence.total_revenue_yoy,
+        qi_a_index_defence.net_income_yoy,
+        growth.total_revenue_yoy_ratio,
+        growth.net_income_yoy_ratio,
+        peg.peg_by_net_income,
+        peg.peg_by_revenue,
         daily_index.close,
         daily_index.pe_ttm,
         daily_index.dividend_ratio,
@@ -65,6 +91,10 @@ final as (
         on stock.stock_code = qi_a_index_defence.stock_code
     left join daily_index
         on stock.stock_code = daily_index.stock_code
+    left join growth
+        on stock.stock_code = growth.stock_code
+    left join peg
+        on stock.stock_code = peg.stock_code
 )
 
 select * from final
