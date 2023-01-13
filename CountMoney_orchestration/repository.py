d498135762fa21a7908ad3b1afd25b71820479fd
c9-killer <54168296+flyanakin@users.noreply.github.com>
2023-01-13 17:43:24 +0800

    任务成功&失败提示 (#10)
    Co-authored-by: ziwei

--- CountMoney_orchestration/repository.py
+++ CountMoney_orchestration/repository.py
@@ -18,6 +18,10 @@ from CountMoney_orchestration.resources import (
     DBT_PROFILES_DIR,
     DBT_PROJECT_DIR,
 )
+from CountMoney_orchestration.ops.hooks import (
+    wecom_bot_message_on_success,
+    wecom_bot_message_on_failure,
+)
 
 
 @repository
@@ -35,14 +39,21 @@ def CountMoney_orchestration():
         resource_defs=resources_prod,
     )
 
-    all_assets = [dbt_assets, tushare_assets]
+    # 整合成所有的assets
+    # 这里需要注意的是，都需要加星号，星号的意识是将数组平铺成一个个元素，最终的效果是all_assets是List of AssetsDefinition类型
+    all_assets = [*dbt_assets, *tushare_assets]
 
-    all_jobs = [demo, daily_assets_job]
+    ##resolve成JobDefinition
+    daily_assets_job_resolved = daily_assets_job.resolve(
+        assets=all_assets, source_assets=[]
+    ).with_hooks(hook_defs={wecom_bot_message_on_success, wecom_bot_message_on_failure})
+
+    all_jobs = [demo, daily_assets_job_resolved]
 
     all_schedules = [
         ScheduleDefinition(
             cron_schedule="0 18 * * *",
-            job=daily_assets_job,
+            job=daily_assets_job_resolved,
             environment_vars=dict(os.environ),
             execution_timezone="Asia/Shanghai",
             default_status=DefaultScheduleStatus.RUNNING,
