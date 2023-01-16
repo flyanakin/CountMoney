cfd3504ab3fc7f1320d600f4009c7deb40fd06c6
c9-killer <54168296+flyanakin@users.noreply.github.com>
2023-01-17 02:38:19 +0800

    feat():支持止盈提醒 (#16)
    Co-authored-by: ziwei

--- CountMoney_orchestration/jobs/daily_push_job.py
+++ CountMoney_orchestration/jobs/daily_push_job.py
@@ -1,15 +1,7 @@
-from dagster import (
-    graph,
-    asset_sensor,
-    RunRequest,
-    AssetKey,
-    EventLogEntry,
-    SensorEvaluationContext,
-)
+from dagster import graph
 from CountMoney_orchestration.ops.analysis import read_table, portfolio_analysis
 from CountMoney_orchestration.ops.push import (
-    send_email,
-    send_wecom_bot,
+    send_wecom_bot,send_wecom_bot_queue
 )
 
 default_config = {
@@ -28,7 +20,7 @@ default_config = {
 @graph()
 def push_analysis_result():
     message = portfolio_analysis(read_table())
-    send_wecom_bot(message)
+    send_wecom_bot_queue(message)
 
 
 daily_push_job = push_analysis_result.to_job(
--- CountMoney_orchestration/ops/analysis.py
+++ CountMoney_orchestration/ops/analysis.py
@@ -1,12 +1,14 @@
 import pandas as pd
+import tushare as ts
 from dagster import op
 from sqlalchemy import create_engine
-
+from CountMoney_orchestration.utils.trade import moving_sell_strategy,turnover_sell_strategy
 ##因为inputManager功能还在实验阶段，先用强撸的方式实现
 from CountMoney_orchestration.resources import (
     WAREHOUSE_USER,
     WAREHOUSE_HOSTS,
     WAREHOUSE_SECRET,
+TUSHARE_TOKEN
 )
 
 
@@ -37,6 +39,8 @@ def read_table(context):
 
 @op()
 def portfolio_analysis(context, portfolio: pd.DataFrame):
+
+    pro = ts.pro_api(f"{TUSHARE_TOKEN}")
     ## 按组合group by，并计算组合收益
     pivoted_to_p = portfolio.groupby(["sub_portfolio"]).sum()
     pivoted_to_p['sub_portfolio_profit'] = (
@@ -55,11 +59,26 @@ def portfolio_analysis(context, portfolio: pd.DataFrame):
             if row.sub_portfolio_profit < -0.08:
                 cut_str = f"{row.Index}组合（{round(row.sub_portfolio_profit*100, 2)}%）"
                 cut_l.append(cut_str)
-                cut_loss_str = "、".join(cut_l)
             else:
                 cut_loss_str = '无'
+    cut_loss_str = "、".join(cut_l)
 
     ##止盈计算
+    sell_result = ""
+    turn_result = ""
+    for index,value in portfolio.iterrows():
+        if value.profit > 0:
+            profit = round(value.profit_ratio*100,2)
+            stage,signal = moving_sell_strategy(value,pro)
+            context.log.info(f"{value.stock_name} {stage} {signal}")
+            sell_str = f"{value.stock_name}(阶梯{stage}，持仓收益{profit}%，{signal})"
+            sell_result = sell_result + sell_str + "\n"
+            if stage > 0 :
+                turnover_signal,total_mv,turnover_rate = turnover_sell_strategy(value,pro)
+                turn_str = f"{value.stock_name}(持仓收益{profit}%，市值{total_mv}亿,今日换手率{turnover_rate}%，{turnover_signal})"
+                context.log.info(f"{total_mv},{turnover_rate}")
+                turn_result = turn_result + turn_str + "\n"
+
 
     ##组合剪枝计算
     stock_cut_l = []
@@ -68,20 +87,23 @@ def portfolio_analysis(context, portfolio: pd.DataFrame):
             if row.profit_ratio < -0.15:
                 stock_cut_str = f"{row.stock_name}({row.sub_portfolio},{round(row.profit_ratio*100, 2)})"
                 cut_l.append(stock_cut_str)
-                stock_cut_l = "、".join(cut_l)
             else:
                 stock_cut_l = '无'
+    stock_cut_l = "、".join(cut_l)
 
     message = f"""
 组合表现：
 {portfolio_profit_str}
 
 止盈提醒：
-· 无
+· 25/8移动止盈：
+{sell_result}
+· 换手率止盈：
+{turn_result}
 
 止损提醒：
 · 组合止损：{cut_loss_str}
 · 组合剪枝：{stock_cut_l}
-    """
+"""
     context.log.info(message)
     return message
