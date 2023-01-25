67a45cd1ffa9da9dc73271b8e611a5876c13dd85
ziwei <ziwei@datastory.com.cn>
2023-01-26 00:27:14 +0800

    增加去年同期的非经常性损益

--- CountMoney_model/models/intermediate/int_income_pivoted_to_stock.sql
+++ CountMoney_model/models/intermediate/int_income_pivoted_to_stock.sql
@@ -7,6 +7,10 @@ stock as (
     select * from {{ ref('stg_tushare_stock_basic') }}
 ),
 
+indicator as (
+    select * from {{ ref('int_finance_indicator_quarterly') }}
+),
+
 deduplicated as (
     --根据发布日期和更新标志去重
     select * from (
@@ -30,6 +34,16 @@ quarter_ordered as (
     from deduplicated
 ),
 
+indicator_ordered as (
+    select
+        *,
+        row_number() over (
+            partition by stock_code
+            order by end_date desc
+            ) as order_num
+    from indicator
+),
+
 last_4_quarter as (
     --取过去4季财报
     select * from quarter_ordered
@@ -42,6 +56,14 @@ last_year as (
         total_revenue,
         net_income_exclude_minority
     from quarter_ordered
+    where order_num = 5
+),
+
+last_year_indicator as (
+    select
+        stock_code,
+        extraordinary_items
+    from indicator_ordered
     where order_num = 5
 ),
 
@@ -66,16 +88,18 @@ ttm as (
 
 final as (
     select
-        stock.stock_code                      as stock_code,
-        last_quarter.end_date                 as last_end_date,
-        ttm.net_income_exclude_minority       as net_income_ttm,
-        ttm.total_revenue                     as total_revenue_ttm,
-        last_year.total_revenue               as total_revenue_last_year,
-        last_year.net_income_exclude_minority as net_income_last_year
+        stock.stock_code                        as stock_code,
+        last_quarter.end_date                   as last_end_date,
+        ttm.net_income_exclude_minority         as net_income_ttm,
+        ttm.total_revenue                       as total_revenue_ttm,
+        last_year.total_revenue                 as total_revenue_last_year,
+        last_year.net_income_exclude_minority   as net_income_last_year,
+        last_year_indicator.extraordinary_items as extraordinary_items_last_year
     from stock
-    left join ttm on stock.stock_code = ttm.stock_code
-    left join last_year on stock.stock_code = last_year.stock_code
-    left join last_quarter on stock.stock_code = last_quarter.stock_code
+    left join ttm using(stock_code)
+    left join last_year using(stock_code)
+    left join last_quarter using(stock_code)
+    left join last_year_indicator using(stock_code)
 )
 
 select * from final
